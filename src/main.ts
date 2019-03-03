96a725e4313fa64789fbf4cc0d0ecd3dc9d39d37
Brian Hood <hoodwebdev@gmail.com>
2019-03-02 22:22:00 -0600

    Load screen timer/text move.  Multiline textarea.

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,14 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+setTimeout(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+    // Otherise, log the boot error
+  }).catch(err => console.error(err));
+}, 4000);
