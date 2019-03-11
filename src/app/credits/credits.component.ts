45bf685a6afd8c257b05aa432ace9998fbbd3715
Jonathan Smith <iamthejonsmith@gmail.com>
2019-03-10 21:28:45 -0500

    add missing component for credits

---
+++ src/app/credits/credits.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Globals } from '../factory.service';
+
+@Component({
+  selector: 'app-credits',
+  templateUrl: './credits.component.html',
+  styleUrls: ['./credits.component.css']
+})
+export class CreditsComponent implements OnInit {
+  developer: any[] = new Array();
+  developers: any[] = new Array();
+
+  constructor(public globals: Globals) {
+    this.developer = [
+      {
+        id: 'Development Studio',
+        name: 'Creative Native Coding, Inc',
+        email: 'creativenativecodinginc@gmail.com'
+      }, {
+        id: 'Lead Developer',
+        name: 'Brian Hood',
+        email: 'tbd'
+      }, {
+        id: 'Developer 1',
+        name: 'Jonathan Smith',
+        email: 'tbd'
+      }, {
+        id: 'Graphics Designer',
+        name: 'Fritz Ling',
+        email: 'tbd'
+      }
+    ];
+  }
+
+  setGuests() {
+    for (let a = 0; a < (this.developer).length; a++) {
+      this.developers.push(this.developer[a]);
+    }
+  }
+
+  ngOnInit() {
+    this.setGuests();
+  }
+
+}
